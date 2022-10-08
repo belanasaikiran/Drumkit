8256d81b34c9d62ca3bdb46e01e8da8ecb15cf9d
Saikiran Belana <88756154+belanasaikiran@users.noreply.github.com>
2022-10-08 09:17:32 +0530

    learn  keyword

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got Clicked");
-
+    // alert("I got Clicked");
     // What to do when clicked
+
+    // "this"  keyword finds out the identity of the button which triggers the event listener
+    // console.log(this)
+    // console.log(this.innerHTML); //to get  element
+
+    // console.log((this.style.color = "white"));
+    this.style.color = "white";
   });
 }
+// var audio = new Audio("./sounds/tom-1.mp3");
+// audio.play();
